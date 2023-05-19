0637ff05b182b9fe81828c7bfdba71e6dea1a589
Derek <dpt5349@psu.edu>
2023-05-19 00:35:25 -0400

    Add project files.

---
+++ Simple library console system/Simple library console system.cpp
@@ -0,0 +1,12 @@
+// Simple library console system.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Please enter what action you would like to take (view, lend, return, add): ";
+
+	return 0;
+}
