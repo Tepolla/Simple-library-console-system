c50b3d5e8e284567837aa6d8cb5aad69a142b4c7
Derek <dpt5349@psu.edu>
2023-05-19 20:02:58 -0400

    Chunked functions implemented

--- Simple library console system/LibraryManagementSystem.cpp
+++ Simple library console system/LibraryManagementSystem.cpp
@@ -1,19 +1,53 @@
 #include "LibraryManagementSystem.h"
-#include <string>
+//#include <string>
+#include <iostream>
 using namespace std;
 
 void LibraryManagementSystem::add(string name) {
-
+	/*for (int i = 0; i < items[0].size(); i++) {
+		if(items[0][i] == name)
+	}*/
+	for (int i = 0; i < books.size(); i++) {
+		if (books[i] == name) {
+			current[i] += 1;
+			total[i] += 1;
+			return;
+		}
+		books.push_back(name);
+		current.push_back(1);
+		total.push_back(1);
+	}
 }
 
-void LibraryManagementSystem::lend() {
-
+void LibraryManagementSystem::lend(string name) {
+	for (int i = 0; i < books.size(); i++) {
+		if (books[i] == name) {
+			current[i] -= 1;
+			return;
+		}
+	}
+	// error out
 }
 
-void LibraryManagementSystem::relinquish() {
-
+void LibraryManagementSystem::relinquish(string name) {
+	for (int i = 0; i < books.size(); i++) {
+		if (books[i] == name) {
+			if (current[i] >= total[i]) {
+				current[i] = total[i];
+				return;
+			}
+			current[i] += 1;
+			return;
+		}
+	}
 }
 
 void LibraryManagementSystem::view() {
-
+	/*for (int i = 0; i < items[0].size(); i++) {
+		cout << items[0][i] << '\t' << items[1][i] << " / " << items[2][i] << endl;
+	}*/
+	cout << "books\tcurrent / total\n";
+	for (int i = 0; i < books.size(); i++) {
+		cout << books[i] << '\t' << current[i] << " / " << total[i] << endl;
+	}
 }
--- Simple library console system/LibraryManagementSystem.h
+++ Simple library console system/LibraryManagementSystem.h
@@ -4,12 +4,15 @@
 
 class LibraryManagementSystem {
 private:
-	std::vector<std::vector<int>> items;
+	//std::vector<std::vector<std::variant<std::string, int>>> items;
+	vector<string> books;
+	vector<int> current;
+	vector<int> total;
 
 public:
 	void add(string name);
-	void lend();
-	void relinquish();
+	void lend(string name);
+	void relinquish(string name);
 	void view();
 
 	std::vector<std::vector<int>> getItems();
